ab56ad9592a668b1ed899ac65025fa91f3a466c3
Raihan Farhad <raihan@techynaf.com>
2019-09-17 11:49:13 +0600

    Create Footer

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(ProfileTableSeeder::class);
+        $this->call(FooterTableSeeder::class);
     }
 }
