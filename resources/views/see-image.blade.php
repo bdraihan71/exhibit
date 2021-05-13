2bf9dd5ac8d63ab97ad3fbd1c5ffbda8c945581b
Jubaed Prince <jubaedprince@gmail.com>
2021-05-13 17:10:13 +0600

    add float-right to button

--- resources/views/see-image.blade.php
+++ resources/views/see-image.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <body>
-    <a class="btn btn-category btn-outline-primary ml-3" href="/">Back</a><br><br>
+    <a class="btn btn-category btn-outline-primary ml-3 float-right" href="/">Back</a><br><br>
     <img src="{{$image}}"></img>
 </body>
 @include('sections.footer')
